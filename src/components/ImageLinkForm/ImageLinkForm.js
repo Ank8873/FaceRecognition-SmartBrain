a894ebe7d64937eb9f8d7a35c3e2c828053ef7c4
Vimal Jaiswal <jaiswalvimal1@gmail.com>
2020-06-19 01:28:43 +0530

    front UI comleted with responsive bug to fix later

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -4,13 +4,13 @@ import './ImageLinkForm.css';
 const ImageLinkForm = () => {
 	return(
 		<div className=''> 
-			<p className='f3'>
+			<p className='f3 white'>
 			{
 				'This Magic Brain will detect the faces in your pictures. Give it a try :)'
 			} </p>
 			<div className='pa4 br3 form center'>
-			<input className='f4 pa2 b--purple h2 bg-lightest-blue w-80 ' type='text' />
-			<button className='w-20 button1 b--purple dim h2 tc f5   link ph3  dib white bg-dark-blue'> Detect</button>
+			<input className='f4 pa2 b--purple h2 bg-lightest-blue w-50 ' type='text' />
+			<button className='w-20 b--purple dim h2 tc f5 link ph3  dib white bg-dark-blue'> Detect</button>
 			</div>		
 		</div>
 	);
