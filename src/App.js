482169e5fc0ce43682fd3a75761251f31787e102
Vimal Jaiswal <jaiswalvimal1@gmail.com>
2020-06-23 16:53:33 +0530

    button check fix

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation.js';
 import Logo from './components/Logo/Logo.js';
 import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 
 const ParticlesOptions = 
  {
@@ -117,21 +118,55 @@ const ParticlesOptions =
   },
   "retina_detect": true
 }
-  
-function App() {
-  return (
+
+const app = new Clarifai.App({
+ apiKey: 'fec298c2589743d0b0b810ce8b9d5336'
+});
+
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    app.models.predict("a403429f2ddf4b49b307e318f00e528b",
+    "https://samples.clarifai.com/face-det.jpg").then(
+    function(response) {
+      // do something with response
+      console.log(response)
+    },
+    function(err) {
+      // there was an error
+    }
+  );
+  }
+
+  render() {
+    return (
     <div className="App">
       <Particles  className='Particles'
         params = {ParticlesOptions} />
       <Navigation />
       <Logo />
-       <Rank /> 
-      <ImageLinkForm />
+      <Rank /> 
+      <ImageLinkForm 
+        onInputChange={this.onInputChange} 
+        onButtonSubmit={this.onButtonSubmit}
+      />
      
       {/*
       <FaceRecognition />*/}
     </div>
   );
+}
 }
 
 export default App;
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './ImageLinkForm.css';
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange , onButtonSubmit}) => {
 	return(
 		<div className=''> 
 			<p className='f3 white'>
@@ -9,8 +9,8 @@ const ImageLinkForm = () => {
 				'This Magic Brain will detect the faces in your pictures. Give it a try :)'
 			} </p>
 			<div className='pa4 br3 form center'>
-			<input className='f4 pa2 b--purple h2 bg-lightest-blue w-50 ' type='text' />
-			<button className='w-20 b--purple dim h2 tc f5 link ph3  dib white bg-dark-blue'> Detect</button>
+			<input className='f4 pa2 b--purple h2 bg-lightest-blue w-50 ' type='text' onChange={onInputChange}/>
+			<button className='w-20 b--purple dim h2 tc f5 link ph3  dib white bg-dark-blue' onClick={onButtonSubmit}> Detect</button>
 			</div>		
 		</div>
 	);
