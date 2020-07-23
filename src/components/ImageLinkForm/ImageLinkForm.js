569064580806a1e1a5035efdf4814c4fbd2e7698
Vimal Jaiswal <jaiswalvimal1@gmail.com>
2020-07-23 13:31:15 +0530

    image link form view fix

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -9,8 +9,8 @@ const ImageLinkForm = ({ onInputChange , onButtonSubmit}) => {
 				'This Magic Brain will detect the faces in your pictures. Give it a try :)'
 			} </p>
 			<div className='pa4 br3 form center'>
-			<input className='f4 pa2 b--purple h2 bg-lightest-blue w-50 ' type='text' onChange={onInputChange}/>
-			<button className='w-20 b--purple dim h2 tc f5 link ph3  dib white bg-dark-blue' onClick={onButtonSubmit}> Detect</button>
+				<input className='f4 pa2 center h2 b--purple bg-lightest-blue w-70 ' type='text' onChange={onInputChange}/>
+				<button className='w-30 b--purple dim h2 tc f5 link ph3 pv2 dib white bg-dark-blue' onClick={onButtonSubmit}> Detect</button>
 			</div>		
 		</div>
 	);
